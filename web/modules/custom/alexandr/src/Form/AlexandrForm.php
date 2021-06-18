71ff9dbe88509ea7a39ae26e8501c9b76958cb06
alexandr1703 <aleks628202@gmail.com>
2021-06-18 16:14:44 +0300

    Validation Email

--- web/modules/custom/alexandr/src/Form/AlexandrForm.php
+++ web/modules/custom/alexandr/src/Form/AlexandrForm.php
@@ -27,12 +27,33 @@ class AlexandrForm extends FormBase {
     $form['cat'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Your cat’s name:'),
-//      '#required' => TRUE,
+      '#required' => TRUE,
       '#maxlength' => 32,
       '#minlength' => 2,
       '#description' => $this->t('Please fill in the field from 2 to 32 characters'),
     ];
 
+
+    $form['email'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Your email'),
+      '#required' => TRUE,
+      '#description' => $this->t('Only letters, "_" and "-"'),
+      '#ajax' => [
+        'callback' => '::valideEmail',
+        'event' => 'keyup',
+        'progress' => array(
+          'type' => 'throbber',
+          'message' => t('Verifying email..'),
+        ),
+      ],
+      '#prefix' => '<div id = "cats-email">',
+    ];
+
+    $form['email-messages'] = [
+      '#markup' => '<div id="email-messages"></div>',
+      '#weight' => -100,
+    ];
 
     $form['submit'] = [
       '#type' => 'submit',
@@ -52,15 +73,40 @@ class AlexandrForm extends FormBase {
 
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $cat = $form_state->getValue('cat');
+    $email = $form_state->getValue('email');
+    $errorArray = [0, 0];
     if (strlen($cat) == 1) {
       \Drupal::messenger()->addError('The name is too short. Please enter a longer name.');
     }
     if(strlen($cat) >1) {
-      \Drupal::messenger()->addMessage($this->t('Your cat name: %cat', ['%cat' => $cat]));
+//      \Drupal::messenger()->addMessage($this->t('Your cat name: %cat', ['%cat' => $cat]));
+      $errorArray[0]=1;
     }
     if (strlen($cat) == '') {
       \Drupal::messenger()->addError('This field is required!!!');
     }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL) || !preg_match('/^[A-Za-z-_]+[@]+[a-z]+[.]+[a-z]+$/', $email)) {
+      \Drupal::messenger()->addError($this->t('Your email is invalid'));
+    }
+    else{
+//      \Drupal::messenger()->addMessage($this->t('Your email is valid'));
+      $errorArray[1]=1;
+    }
+    if ($errorArray[0]==1 && $errorArray[1]==1){
+      \Drupal::messenger()->addMessage($this->t('Your cat name: %cat', ['%cat' => $cat]));
+    }
+  }
+
+  public function valideEmail(array &$form, FormStateInterface $form_state){
+    $ajax_response = new AjaxResponse();
+    $email = $form_state->getValue('email');
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL) || !preg_match('/^[A-Za-z-_]+[@]+[a-z]+[.]+[a-z]+$/', $email)) {
+      $ajax_response->addCommand(new HtmlCommand('#email-messages', 'This email is invalid'));
+    }
+    else {
+      $ajax_response->addCommand(new HtmlCommand('#email-messages', $email));
+    }
+    return $ajax_response;
   }
 
   public function ajaxSubmitCallback(array &$form, FormStateInterface $form_state) {
